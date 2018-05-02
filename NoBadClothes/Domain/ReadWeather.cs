69d4081ffbfe6db39d4c4471d037cecdda744cdc
Ida Ahlström <ida.ahlstrom@collectorbank.se>
2018-05-02 16:11:59 +0200

    Build weather

--- NoBadClothes/Domain/ReadWeather.cs
+++ NoBadClothes/Domain/ReadWeather.cs
@@ -11,28 +11,26 @@ namespace NoBadClothes
     {
         public Station UpdateStation(Forecast forecast, Station station)
         {
-            
+            return station;
         }
 
-        public Station buildWeather(Forecast forecast, Station station, DateTime time)
+        public void buildWeather(Forecast forecast, Station station, DateTime time)
         {
             Weather weather = new Weather ();
-            DateTime nextFullHour = new DateTime(time.Year, time.Month, time.Day, time.Hour + 1, 0, 0);
 
-            weather.Temperature = forecast.timeSeries.First(t => t.validTime == nextFullHour).parameters.First(p => p.name == "t").values[0];
-            weather.WindDirection = (int)forecast.timeSeries.First(t => t.validTime == nextFullHour).parameters.First(p => p.name == "wd").values[0];
-            weather.WindSpeed = forecast.timeSeries.First(t => t.validTime == nextFullHour).parameters.First(p => p.name == "ws").values[0];
+            weather.Time = time;
+            weather.Temperature = forecast.timeSeries.First(t => t.validTime == time).parameters.First(p => p.name == "t").values[0];
+            weather.WindDirection = (int)forecast.timeSeries.First(t => t.validTime == time).parameters.First(p => p.name == "wd").values[0];
+            weather.WindSpeed = forecast.timeSeries.First(t => t.validTime == time).parameters.First(p => p.name == "ws").values[0];
 
+            weather.WindGust = forecast.timeSeries.First(t => t.validTime == time).parameters.First(p => p.name == "gust").values[0];
 
-            weather.WindGust = forecast.timeSeries.First(t => t.validTime == nextFullHour).parameters.First(p => p.name == "gust").values[0];
-            weather.PrecipationCategory = (PrecipationCategory)forecast.timeSeries.First(t => t.validTime == nextFullHour).parameters.First(p => p.name == "pcat").values[0];
-            weather.WeatherSymbol = (WeatherSymbol)forecast.timeSeries.First(t => t.validTime == nextFullHour).parameters.First(p => p.name == "Wsymb2").values[0];
-            weather.PrecipationMean = forecast.timeSeries.First(t => t.validTime == nextFullHour).parameters.First(p => p.name == "pmean").values[0];
+            weather.PrecipationCategory = (PrecipationCategory)forecast.timeSeries.First(t => t.validTime == time).parameters.First(p => p.name == "pcat").values[0];
+            weather.WeatherSymbol = (WeatherSymbol)forecast.timeSeries.First(t => t.validTime == time).parameters.First(p => p.name == "Wsymb2").values[0];
+            weather.PrecipationMean = forecast.timeSeries.First(t => t.validTime == time).parameters.First(p => p.name == "pmean").values[0];
 
-            station.
+            station.WeatherForecast.Add(weather);
 
-            
-            return station;
 
         }
         //läs Json och skapa objekt
