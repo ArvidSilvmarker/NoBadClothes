ba1a45f985c38d289af294326af9e22cbbe7f38f
Ida Ahlström <ida.ahlstrom@collectorbank.se>
2018-05-02 11:46:18 +0200

    Json anrop

--- NoBadClothes/Domain/GetWeather.cs
+++ NoBadClothes/Domain/GetWeather.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace NoBadClothes
 {
     public class GetWeather
     {
+        public JObject GetJsonForecast(double longitude, double latitude )
+        {
+            var jsonTemp = new WebClient().DownloadString($"https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/{longitude}/lat/{latitude}/data.json");
+            var jsonTempObject = (JObject)JsonConvert.DeserializeObject(jsonTemp);
+            return jsonTempObject;
 
+            var exempel = (string)jsonTempObject["timeSeries"][0]["parameters"][0]["name"];
+            Console.WriteLine(exempel);
+        }
         //hämta Json från SMHI
     }
 }
--- NoBadClothes/Domain/ReadWeather.cs
+++ NoBadClothes/Domain/ReadWeather.cs
@@ -7,6 +7,13 @@ namespace NoBadClothes
 {
     public class ReadWeather
     {
+        public void ReadJson()
+        {
+            double longitude = 57.70887000;
+            double latitude = 11.97456000;
+
+            var weathre = 
+        }
         //läs Json och skapa objekt
     }
 }
