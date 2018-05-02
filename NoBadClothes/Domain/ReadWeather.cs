8ec528241b060a2a1f84949c04b22357446024fd
Ida Ahlström <ida.ahlstrom@collectorbank.se>
2018-05-02 13:43:30 +0200

    väder

--- NoBadClothes/Domain/ReadWeather.cs
+++ NoBadClothes/Domain/ReadWeather.cs
@@ -10,8 +10,17 @@ namespace NoBadClothes
     {
         public Station UpdateStation(JObject jObject, Station station)
         {
-            var example = (string)jObject["timeSeries"][0]["parameters"][0]["name"];
-            return example;
+            DateTime referenceTime = (DateTime)jObject["referenceTime"];
+
+            var temperature = (double)jObject["timeSeries"][0]["parameters"][11]["values"][0];
+            var windDirection = (int)jObject["timeSeries"][0]["parameters"][13]["values"][0];
+            var windSpeed = (double)jObject["timeSeries"][0]["parameters"][14]["values"][0];
+            var WindGust = (string)jObject["timeSeries"][0]["parameters"][17]["values"][0];
+            var WindGust = (string)jObject["timeSeries"][0]["parameters"][17]["values"][0];
+
+
+            station.MorningWeather.Temperature = temperature;
+            return station;
         }
         //läs Json och skapa objekt
     }
