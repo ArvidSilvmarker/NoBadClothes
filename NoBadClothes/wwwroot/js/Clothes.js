15185411a2470b1c380209d0b21596a4c880bb56
Ida Ahlström <ida.ahlstrom@collectorbank.se>
2018-05-03 22:56:09 +0200

    Det slutade fungera

--- NoBadClothes/wwwroot/js/Clothes.js
+++ NoBadClothes/wwwroot/js/Clothes.js
@@ -1,27 +1,32 @@
 ﻿$("#whatButton").click(function () {
     alert("första");
-
-
     var h = $("#hour").val();
     var d = $("#dateTime").val();
     var c = $("#cityName").val();
 
-    $.ajax({
-        url: "/api/clothes/getClothes/",
-        data: {
-            hour: h,
-            dateTime : d,
-            cityName : c}
 
-        })
-        .done(function (result) {
-            ("#text").html("<h2> Ha!.</h2>");
 
-            alert("hello");
+    location.href = $"/api/clothes/getClothes?hour={h}&dateTime={d}&cityName={c}";
 
-        })
-        .fail(function (xhr, status, error) {
+    //;
 
-            alert("error");
-        });
+
+    //$.ajax({
+    //    url: "/api/clothes/getClothes/",
+    //    data: {
+    //        hour: h,
+    //        dateTime : d,
+    //        cityName : c}
+
+    //    })
+    //    .done(function (result) {
+    //        ("#text").html("<h2> Ha!.</h2>");
+
+    //        alert("hello");
+
+    //    })
+    //    .fail(function (xhr, status, error) {
+
+    //        alert("error");
+    //    });
 });
