2735ea71ef0337a579afc88dca982db162000dbd
Ida Ahlström <ida.ahlstrom@collectorbank.se>
2018-05-03 21:46:39 +0200

    HTML

---
+++ NoBadClothes/wwwroot/js/Clothes.js
@@ -0,0 +1,27 @@
+﻿$("#whatButton").click(function () {
+    alert("första");
+
+
+    var h = $("#hour").val();
+    var d = $("#dateTime").val();
+    var c = $("#cityName").val();
+
+    $.ajax({
+        url: "/api/clothes/getClothes/",
+        data: {
+            hour: h,
+            dateTime : d,
+            cityName : c}
+
+        })
+        .done(function (result) {
+            ("#text").html("<h2> Ha!.</h2>");
+
+            alert("hello");
+
+        })
+        .fail(function (xhr, status, error) {
+
+            alert("error");
+        });
+});
