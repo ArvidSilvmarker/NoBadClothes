bf3beda7e36dc0d699f637ecd9da33d33581f811
Ida Ahlström <ida.ahlstrom@collectorbank.se>
2018-05-04 08:55:27 +0200

    clothes hemida

--- NoBadClothes/wwwroot/js/Clothes.js
+++ NoBadClothes/wwwroot/js/Clothes.js
@@ -1,32 +1,29 @@
 ﻿$("#whatButton").click(function () {
+
     alert("första");
     var h = $("#hour").val();
     var d = $("#dateTime").val();
     var c = $("#cityName").val();
 
 
-
-    location.href = $"/api/clothes/getClothes?hour={h}&dateTime={d}&cityName={c}";
-
-    //;
+    //location.href = "/api/clothes/getClothes?hour={h}&dateTime={d}&cityName={c}";
 
 
-    //$.ajax({
-    //    url: "/api/clothes/getClothes/",
-    //    data: {
-    //        hour: h,
-    //        dateTime : d,
-    //        cityName : c}
+    $.ajax({
+        url: "/api/clothes/getClothes/",
+        data: {
+            hour: h,
+            dateTime : d,
+            cityName : c}
 
-    //    })
-    //    .done(function (result) {
-    //        ("#text").html("<h2> Ha!.</h2>");
+        })
+        .done(function (result) {
+            ("#text").html("<h2> Ha!.</h2>");
+            alert("hello");
 
-    //        alert("hello");
+        })
+        .fail(function (xhr, status, error) {
 
-    //    })
-    //    .fail(function (xhr, status, error) {
-
-    //        alert("error");
-    //    });
+            alert("error");
+        });
 });
