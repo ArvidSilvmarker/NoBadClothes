3cf9f30d71c2d004fe551f7f302ee0a82f85b7ef
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-05-07 15:21:05 +0200

    no message

--- NoBadClothes/wwwroot/js/Weather.js
+++ NoBadClothes/wwwroot/js/Weather.js
@@ -1,1 +1,15 @@
-﻿
+﻿$("#seedButton").click(function () {
+    $.ajax({
+            url: "/api/weather/seedtopten",
+            method: "POST"
+        })
+        .done(function (result) {
+            alert(result);
+        })
+        .fail(function (xhr, status, error) {
+            alert(`Error! ${xhr.responseText}`);
+            console.log("xhr", xhr);
+            console.log("status", status);
+            console.log("error", error);
+        });
+});
